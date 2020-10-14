fcb44b47fdfcbd52de8a30430890f1b0399b2fc9
3Emme <emmettbuentiempo@gmail.com>
2020-10-14 14:02:41 -0700

    update views to match each other and migrate with new behavior model.
    Co-Authored By: Garrett Brown <gman9mm@live.com>
    Co-Authored-by: Ben Stoller <stollersystemsound@gmail.com>

---
+++ RpgCompendium/Models/Behavior.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace RpgCompendium.Models
+{
+  public class Behavior
+  {
+    public int BehaviorId { get; set; }
+    public string BehaviorName { get; set; }
+    public string BehaviorDescription { get; set; }
+  }
+}
--- RpgCompendium/Models/RpgCompendiumContext.cs
+++ RpgCompendium/Models/RpgCompendiumContext.cs
@@ -5,7 +5,8 @@ namespace RpgCompendium.Models
   public class RpgCompendiumContext : DbContext
   {
     public virtual DbSet<Monster> Monsters { get; set; }
-    public virtual DbSet<MainType> MainTypes { get; set; }    
+    public virtual DbSet<MainType> MainTypes { get; set; }
+    public virtual DbSet<Behavior> Behaviors { get; set; }
 
     public RpgCompendiumContext(DbContextOptions options) : base(options) { }
   }
